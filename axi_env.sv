596d726f1ce4601baf1cb821f52da83c3e4316d0
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-06-03 11:06:23 -0700

    Enhance environment with virtual sequencer hookup

--- axi_env.sv
+++ axi_env.sv
@@ -1,7 +1,6 @@
 //----------------------------------------------------------------
-// AXI Environment
-// - Top level reusable UVM environment for AXI verification
-// - Instantiates both read and write agents
+// AXI Environment (Enhanced)
+// - Adds virtual sequencer to co-ordinate write and read agents
 //----------------------------------------------------------------
 class axi_env extends uvm_env;
     `uvm_component_utils(axi_env)
@@ -13,6 +12,9 @@ class axi_env extends uvm_env;
     //Scoreboard
     axi_scoreboard  scoreboard;
 
+    //Virtual Sequencer
+    axi_virtual_sequencer virt_sequencer;
+
     //Constructor
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -22,9 +24,10 @@ class axi_env extends uvm_env;
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
-        write_agent = axi_write_agent::type_id::create("write_agent", this);
-        read_agent  = axi_read_agent::type_id::create("read_agent", this);
-        scoreboard  = axi_scoreboard::type_id::create("scoreboard", this);
+        write_agent    = axi_write_agent::type_id::create("write_agent", this);
+        read_agent     = axi_read_agent::type_id::create("read_agent", this);
+        scoreboard     = axi_scoreboard::type_id::create("scoreboard", this);
+        virt_sequencer = axi_virtual_sequencer::type_id::create("virt_sequencer", this);
 
     //You can set ACTIVE/PASSIVE externally or in the test
     endfunction
@@ -35,5 +38,9 @@ class axi_env extends uvm_env;
 
         write_agent.mon.mon_ap.connect(scoreboard.write_export);
         read_agent.mon.mon_ap.connect(scoreboard.read_export);
+
+        //Virtual Sequencer hookup
+        virt_sequencer.write_sequencer = write_agent.seqr;
+        virt_sequencer.read_sequencer  = read_agent.seqr;
     endfunction
 endclass
