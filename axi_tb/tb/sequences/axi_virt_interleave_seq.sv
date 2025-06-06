cfab12591ec4a109d473d6f7de5d8258695a5878
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-06-05 19:18:13 -0700

    AXI Virtual interleave sequence

--- axi_tb/tb/sequences/axi_virt_interleave_seq.sv
+++ axi_tb/tb/sequences/axi_virt_interleave_seq.sv
@@ -20,10 +20,15 @@ class axi_virt_interleave_seq extends uvm_sequence;
         axi_read_seq read_seq;
 
         for(i = 0; i < 4; i++) begin
+            //Randomize a valid word-aligned address in 0x00 - 0xFF
+            assert(std::randomize(addr) with {addr inside {[32'h00 : 32'hFF]}; addr[1:0] == 2'b00});
+
+            //WRITE
             `uvm_info("VSEQ", $sformatf("Starting WRITE[%0d]", i), UVM_MEDIUM)
               write_seq = axi_write_seq::type_id::create("write_seq");
               write_seq.start(p_sequencer.write_sequencer);
 
+            //READ
              `uvm_info("VSEQ", $sformatf("Starting READ[%0d]", i), UVM_MEDIUM)  
                read_seq  = axi_read_seq::type_id::create("read_seq");
                read_seq.start(p_sequencer.read_sequencer);
