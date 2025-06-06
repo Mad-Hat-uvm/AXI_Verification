f26db7f560c6360da3ccb2a53e6b951cbe76df9e
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-06-05 18:39:28 -0700

    axi_write_only_test

--- axi_tb/tests/axi_write_only_test.sv
+++ axi_tb/tests/axi_write_only_test.sv
@@ -19,8 +19,6 @@ class axi_write_only_test extends axi_base_test;
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
-        env = axi_env::type_id::create("env", this);
-
         //Activate only the write agent
         uvm_config_db#(uvm_active_passive_enum)::set(this, "env.write_agent", "is_active", UVM_ACTIVE);
         uvm_config_db#(uvm_active_passive_enum)::set(this, "env.read_agent", "is_active", UVM_PASSIVE);
@@ -28,7 +26,7 @@ class axi_write_only_test extends axi_base_test;
 
     //Run the sequence
     task run_phase(uvm_phase phase);
-        axi_write_seq write_seq;
+        axi_write_seq seq;
 
         phase.raise_objection(this);
         //Create the write sequence
